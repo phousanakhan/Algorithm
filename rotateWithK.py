99238498027a097a8c199d9720c06663d65a2aca
Phousanak Han <phoukao200@gmail.com>
2020-07-02 16:19:59 -0600

    roateWithK now don't exceed time limit

--- rotateWithK.py
+++ rotateWithK.py
@@ -1,6 +1,7 @@
-class Solution:
-    def rotate(self, nums: List[int], k: int) -> None:
-        for i in range(k):
-            previous = nums[-1]
-            for j in range(len(nums)):
-                nums[j], previous = previous, nums[j]
+def rotate(self, nums: List[int], k: int) -> None:
+    toRotate = 0
+    while toRotate != k:
+        tempList = (nums[0:-1])
+        del nums[:-1]
+        nums.extend(tempList)
+        toRotate += 1
