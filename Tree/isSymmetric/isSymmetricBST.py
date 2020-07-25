7e8504aab23ecc816f721fe21a081be864e4da72
Phousanak Han <phoukao200@gmail.com>
2020-07-25 13:27:10 -0600

    BST isSymmetric

---
+++ Tree/isSymmetric/isSymmetricBST.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        if root == None:
+            return(True)
+        else:
+            return self.isMirror(root.left, root.right)
+    
+    def isMirror(self, root1: TreeNode, root2: TreeNode):
+        try:
+            if root1 == None and root2 == None:
+                return True 
+            if root1.val != root2.val:
+                return False
+            else:
+                return self.isMirror(root1.left, root2.right) and  self.isMirror(root1.right, root2.left)
+        except:
+            return(False)
