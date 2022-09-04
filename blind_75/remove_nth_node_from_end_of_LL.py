9acc350ccb0fa5f755724868930be5ab7a3852ff
Phousanak Han <phousana@ualberta.ca>
2022-09-04 17:44:10 -0600

    remove nth node from end of LL

---
+++ blind_75/remove_nth_node_from_end_of_LL.py
@@ -0,0 +1,41 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        '''
+        1. Get the length
+        2. Calculate preMove & toDelete
+        3. set preMove.next = toDelete.next
+        '''
+        
+        #Get the length
+        length = 0
+        tempHead = head
+        while tempHead != None:
+            tempHead = tempHead.next
+            length += 1
+        
+        #Get preMove & toDelete
+        preMove = None
+        toDelete = None
+        tempHead1 = head
+        target = length - n
+        i = 0
+        while i != target:
+            preMove = tempHead1
+            tempHead1 = tempHead1.next
+            toDelete = tempHead1
+            i += 1
+            
+        #Set preMove.next = toDelete.next
+        if preMove == None: #this means nth node is the first element of the list
+            #return list excluding the first element
+            return head.next
+        else:
+            #3->4->5
+            #3->5
+            preMove.next = toDelete.next
+        return head
